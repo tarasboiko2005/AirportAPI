5025c68151dfbcbb94e1b03a9e019cc934e8c8f3
Taras Boiko <tarasboiko2005@gmail.com>
2025-12-08 13:11:01 +0200

    Refactor views and urls: split generic views, add serializers to APIViews

--- aviation/core/urls.py
+++ aviation/core/urls.py
@@ -1,22 +1,44 @@
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 import debug_toolbar
 from .views import (
-    UserViewSet, CountryViewSet, AirportViewSet,
-    AirlineViewSet, AirplaneViewSet, FlightViewSet, TicketViewSet
+    # ViewSets
+    UserViewSet, FlightViewSet, AirplaneViewSet,
+
+    # Generic Views (розділені)
+    CountryListCreateView, CountryDetailView,
+    AirportListCreateView, AirportDetailView,
+
+    # APIViews
+    AirlineListView, AirlineDetailView,
+    TicketListView, TicketDetailView,
 )
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'countries', CountryViewSet)
-router.register(r'airports', AirportViewSet)
-router.register(r'airlines', AirlineViewSet)
-router.register(r'airplanes', AirplaneViewSet)
 router.register(r'flights', FlightViewSet)
-router.register(r'tickets', TicketViewSet)
+router.register(r'airplanes', AirplaneViewSet)
 
 urlpatterns = [
+    # ViewSets через router
     path('', include(router.urls)),
+
+    # Countries через GenericAPIView
+    path('countries-generic/', CountryListCreateView.as_view(), name='countries-generic'),
+    path('countries-generic/<int:pk>/', CountryDetailView.as_view(), name='country-generic-detail'),
+
+    # Airports через GenericAPIView
+    path('airports-generic/', AirportListCreateView.as_view(), name='airports-generic'),
+    path('airports-generic/<int:pk>/', AirportDetailView.as_view(), name='airport-generic-detail'),
+
+    # Airlines через APIView
+    path('airlines-view/', AirlineListView.as_view(), name='airlines-view'),
+    path('airlines-view/<int:pk>/', AirlineDetailView.as_view(), name='airline-view-detail'),
+
+    # Tickets через APIView
+    path('tickets-view/', TicketListView.as_view(), name='tickets-view'),
+    path('tickets-view/<int:pk>/', TicketDetailView.as_view(), name='ticket-view-detail'),
+
+    # Debug toolbar
     path('__debug__/', include(debug_toolbar.urls)),
 ]
--- aviation/core/views.py
+++ aviation/core/views.py
@@ -1,10 +1,14 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from drf_spectacular.utils import extend_schema_view, extend_schema
-from rest_framework import generics
+from rest_framework import generics, mixins
+from django.shortcuts import get_object_or_404
 
 from .models import User, Country, Airport, Airline, Airplane, Flight, Ticket
 from .serializers import (
@@ -32,6 +36,7 @@ class LoginView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
         return super().post(request, *args, **kwargs)
 
+# Users through ViewSet
 @extend_schema_view(
     list=extend_schema(tags=['Users']),
     retrieve=extend_schema(tags=['Users']),
@@ -44,54 +49,128 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-@extend_schema_view(
-    list=extend_schema(tags=['Countries']),
-    retrieve=extend_schema(tags=['Countries']),
-    create=extend_schema(tags=['Countries']),
-    update=extend_schema(tags=['Countries']),
-    partial_update=extend_schema(tags=['Countries']),
-    destroy=extend_schema(tags=['Countries']),
-)
-class CountryViewSet(viewsets.ModelViewSet):
+# Country through Generics Views
+@extend_schema(tags=['Countries'])
+class CountryListCreateView(
+    generics.GenericAPIView,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
     queryset = Country.objects.all()
     serializer_class = CountrySerializer
 
-@extend_schema_view(
-    list=extend_schema(tags=['Airports']),
-    retrieve=extend_schema(tags=['Airports']),
-    create=extend_schema(tags=['Airports']),
-    update=extend_schema(tags=['Airports']),
-    partial_update=extend_schema(tags=['Airports']),
-    destroy=extend_schema(tags=['Airports']),
-)
-class AirportViewSet(viewsets.ModelViewSet):
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+@extend_schema(tags=['Countries'])
+class CountryDetailView(
+    generics.GenericAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+    def get(self, request, pk, *args, **kwargs):
+        return self.retrieve(request, pk=pk, *args, **kwargs)
+
+    def put(self, request, pk, *args, **kwargs):
+        return self.update(request, pk=pk, *args, **kwargs)
+
+    def patch(self, request, pk, *args, **kwargs):
+        return self.partial_update(request, pk=pk, *args, **kwargs)
+
+    def delete(self, request, pk, *args, **kwargs):
+        return self.destroy(request, pk=pk, *args, **kwargs)
+
+# Airport through Generic Views
+@extend_schema(tags=['Airports'])
+class AirportListCreateView(
+    generics.GenericAPIView,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
     queryset = Airport.objects.select_related('country').all()
     serializer_class = AirportSerializer
 
-@extend_schema_view(
-    list=extend_schema(tags=['Airlines']),
-    retrieve=extend_schema(tags=['Airlines']),
-    create=extend_schema(tags=['Airlines']),
-    update=extend_schema(tags=['Airlines']),
-    partial_update=extend_schema(tags=['Airlines']),
-    destroy=extend_schema(tags=['Airlines']),
-)
-class AirlineViewSet(viewsets.ModelViewSet):
-    queryset = Airline.objects.select_related('airport').all()
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+@extend_schema(tags=['Airports'])
+class AirportDetailView(
+    generics.GenericAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+):
+    queryset = Airport.objects.select_related('country').all()
+    serializer_class = AirportSerializer
+
+    def get(self, request, pk, *args, **kwargs):
+        return self.retrieve(request, pk=pk, *args, **kwargs)
+
+    def put(self, request, pk, *args, **kwargs):
+        return self.update(request, pk=pk, *args, **kwargs)
+
+    def patch(self, request, pk, *args, **kwargs):
+        return self.partial_update(request, pk=pk, *args, **kwargs)
+
+    def delete(self, request, pk, *args, **kwargs):
+        return self.destroy(request, pk=pk, *args, **kwargs)
+
+# Airlines through APIView
+@extend_schema(tags=['Airlines'])
+class AirlineListView(APIView):
     serializer_class = AirlineSerializer
+    def get(self, request):
+        airlines = Airline.objects.all()
+        serializer = AirlineSerializer(airlines, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-@extend_schema_view(
-    list=extend_schema(tags=['Airplanes']),
-    retrieve=extend_schema(tags=['Airplanes']),
-    create=extend_schema(tags=['Airplanes']),
-    update=extend_schema(tags=['Airplanes']),
-    partial_update=extend_schema(tags=['Airplanes']),
-    destroy=extend_schema(tags=['Airplanes']),
-)
-class AirplaneViewSet(viewsets.ModelViewSet):
-    queryset = Airplane.objects.select_related('airline').all()
-    serializer_class = AirplaneSerializer
+    def post(self, request):
+        serializer = AirlineSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@extend_schema(tags=['Airlines'])
+class AirlineDetailView(APIView):
+    serializer_class = AirlineSerializer
+    def get(self, request, pk):
+        airline = get_object_or_404(Airline, pk=pk)
+        serializer = AirlineSerializer(airline)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        airline = get_object_or_404(Airline, pk=pk)
+        serializer = AirlineSerializer(airline, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        airline = get_object_or_404(Airline, pk=pk)
+        serializer = AirlineSerializer(airline, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        airline = get_object_or_404(Airline, pk=pk)
+        airline.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Flights through ViewSet
 @extend_schema_view(
     list=extend_schema(tags=['Flights']),
     retrieve=extend_schema(tags=['Flights']),
@@ -107,17 +186,62 @@ class FlightViewSet(viewsets.ModelViewSet):
     filterset_fields = ['origin', 'destination', 'status', 'airplane']
     permission_classes = [IsAuthenticated]
 
-@extend_schema_view(
-    list=extend_schema(tags=['Tickets']),
-    retrieve=extend_schema(tags=['Tickets']),
-    create=extend_schema(tags=['Tickets']),
-    update=extend_schema(tags=['Tickets']),
-    partial_update=extend_schema(tags=['Tickets']),
-    destroy=extend_schema(tags=['Tickets']),
-)
-class TicketViewSet(viewsets.ModelViewSet):
-    queryset = Ticket.objects.select_related('flight', 'user').all()
+#Tickets through APIView
+@extend_schema(tags=['Tickets'])
+class TicketListView(APIView):
     serializer_class = TicketSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['flight', 'user', 'status']
-    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        tickets = Ticket.objects.select_related('flight', 'user').all()
+        serializer = TicketSerializer(tickets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TicketSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@extend_schema(tags=['Tickets'])
+class TicketDetailView(APIView):
+    serializer_class = TicketSerializer
+    def get(self, request, pk):
+        ticket = get_object_or_404(Ticket, pk=pk)
+        serializer = TicketSerializer(ticket)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        ticket = get_object_or_404(Ticket, pk=pk)
+        serializer = TicketSerializer(ticket, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        ticket = get_object_or_404(Ticket, pk=pk)
+        serializer = TicketSerializer(ticket, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        ticket = get_object_or_404(Ticket, pk=pk)
+        ticket.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#Airplane through viewset
+@extend_schema_view(
+    list=extend_schema(tags=['Airplanes']),
+    retrieve=extend_schema(tags=['Airplanes']),
+    create=extend_schema(tags=['Airplanes']),
+    update=extend_schema(tags=['Airplanes']),
+    partial_update=extend_schema(tags=['Airplanes']),
+    destroy=extend_schema(tags=['Airplanes']),
+)
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.select_related('airline').all()
+    serializer_class = AirplaneSerializer
+
+
