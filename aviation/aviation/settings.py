8589973c36ec04677a3da7813cfc62fd2bfc0dd2
Taras Boiko <tarasboiko2005@gmail.com>
2026-01-06 11:03:55 +0200

    added redis to render.yaml

--- aviation/aviation/settings.py
+++ aviation/aviation/settings.py
@@ -204,7 +204,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
+            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
         },
     },
 }
